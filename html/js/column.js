b3eb5743ba1fc6a258399f96bc310f920f4ff276
James Holland <jholland@usgs.gov>
2013-07-08 15:06:56 -0600

    Added list of checkboxes (No labels)

--- html/js/column.js
+++ html/js/column.js
@@ -5,8 +5,57 @@ column.js contains functions for column hiding and showing.
 License: Public Domain
 */
 
-
-function setupColumnTab(jTab, curTab){
+var colVis;
+function setupColumnTab(jTab){
     var eTab = $("<div id='tColumn' style=\"font-size: 12px\"></div>");
+    jTab.append(eTab);
     jTab.tabs("add", "#tColumn", "Column");
+
+    var maxCol = 4;
+    var colWidth = 460; //The size during development was 451px. 460px gives spacing between columns.
+    var numCol = Math.floor($(document).width()/colWidth);
+    if(numCol > maxCol){  //We don't want more than 3 columns.
+        numCol = maxCol;
+    }
+    var metrics = dTable.fnSettings().aoColumns;
+    var numPerCol = Math.ceil((metrics.length + 1)/numCol);
+    var curMetric = 0;
+    var curCol = -1;
+    var columns = [];
+    for(; curMetric < metrics.length; curMetric++){
+        if(!(curMetric % numPerCol)){
+            curCol++;
+            columns.push($("<div style=\"display:table; border-spacing:10px;\"></div>"));
+        }
+        columns[curCol].append(createCheckbox(metrics[curMetric], curMetric));
+    }
+    var colTable = $("<div style=\"display:table; border-spacing: 9px;\"></div>");
+    for(var i = 0; i<columns.length; i++){
+        var col = $("<div style=\"display:table-cell;\"></div>");
+        col.append(columns[i]);
+        colTable.append(col);
+    }
+    eTab.append(colTable);
+//    jTab.on( "tabsactivate", function(event, ui){
+    //colVis.fnRebuild();
+//    });
+}
+/*    wTab.append(colTable);
+    wTab.append(   
+        "<button type='button' id='btnWeightReset'>Reset Weights</button>"
+    );
+    wTab.append(   
+        "<button type='button' id='btnWeightSetZero'>Zero Weights</button>"
+    );
+    wTab.append(   
+        "<button type='button' id='btnWeightProcessAggr'>Recompute Aggregrate</button>"
+    );
+    jTab.append(wTab);
+    jTab.tabs("add", "#tWeight", "Weights");
+    bindWeightTab();*/
+
+function createCheckbox(metricCol, colID){
+    var cbdiv = $("<div id='metricCB"+colID+"'/>");
+    cbdiv.append($("<input type='checkbox'/>"));
+    return cbdiv;
 }
