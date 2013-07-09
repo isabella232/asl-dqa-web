b843df0abae5728db5513868794dd47c25211ef6
James Holland <jholland@usgs.gov>
2013-07-08 18:58:14 -0600

    Refactored some code and added comments about why somethings are set the way they are.

--- html/js/column.js
+++ html/js/column.js
@@ -12,23 +12,24 @@ function setupColumnTab(jTab){
     jTab.tabs("add", "#tColumn", "Column");
 
     var maxCol = 4;
-    var colWidth = 460; //The size during development was 451px. 460px gives spacing between columns.
+    var colWidth = 460; //This size was left over from weight columns.
     var numCol = Math.floor($(document).width()/colWidth);
     if(numCol > maxCol){  //We don't want more than 3 columns.
         numCol = maxCol;
     }
     var metrics = dTable.fnSettings().aoColumns;
     var numPerCol = Math.ceil((metrics.length + 1)/numCol);
-    var curMetric = 0;
-    var curCol = -1;
+    var curCol = -1; //If not -1 columns will skip 0
     var columns = [];
-    for(; curMetric < metrics.length; curMetric++){
-        if(!(curMetric % numPerCol)){
+    var usedMetrics = 0; //Tracks how many metrics are being displayed.
+    for(var curMetric = 0; curMetric < metrics.length; curMetric++){
+        if(!(usedMetrics % numPerCol)){
             curCol++;
             columns.push($("<div style=\"display:table; border-spacing:10px;\"></div>"));
         }
         if(metrics[curMetric].bVisible){  //If the column is hidden by default we don't want to let users view it. This applies to the groups column which is used for filtering.
             columns[curCol].append(createCheckbox(metrics[curMetric], curMetric));
+            usedMetrics++;
         }
     }
     var colTable = $("<div style=\"display:table; border-spacing: 9px;\"></div>");
