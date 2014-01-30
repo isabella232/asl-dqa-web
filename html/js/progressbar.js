036455a281a242be89ff40e20c014091ab389fc5
James Holland <jholland@usgs.gov>
2014-01-29 17:35:33 -0700

    Adding progress bar. It only works on initial load currently, subsequent updates cause it to hang.

---
+++ html/js/progressbar.js
@@ -0,0 +1,31 @@
+/*
+progressbar.js
+Author: James Holland jholland@usgs.gov
+progressbar.js contains functions for displaying a progress bar during ajax queries.
+License: Public Domain
+*/
+
+$(function() {
+var progress = $("#dlgprogressbar").dialog({
+    height: $(document).height(),
+    width: $(document).width(),
+    resizable: false,
+    overlay: {
+        opacity: 0.5,
+        background: "black"
+    },
+    closeOnEscape: false,
+    dialogClass: 'noTitle noBackGround',
+    modal: true
+});
+progress.append($('<div id="progressbar"><div class="progress-label">Loading...</div></div>').progressbar({value: false}));
+progress.dialog("moveToTop");
+$(document)
+.ajaxStart(function () {
+    progress.dialog("open");
+    progress.dialog("moveToTop");
+})
+.ajaxStop(function () {
+    progress.dialog("close");
+});
+});
