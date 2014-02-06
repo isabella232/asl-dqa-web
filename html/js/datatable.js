da4f6ec59160110f07722a7a17a794f3d6c34953
James Holland <jholland@usgs.gov>
2014-02-05 18:19:25 -0700

    Removed global:false tags from data get requests. This was left over from initial loading bar setup and broke the weighttab setup. This causes the loading bar to persist until all data is loaded the first time,but this is acceptable as it ensures stability.

--- html/js/datatable.js
+++ html/js/datatable.js
@@ -173,8 +173,7 @@ function fillTable(){
                         },
                         url: "cgi-bin/metrics.py",
                         data: {cmd: "stationgrid", param: "station."+rowIDs+
-                            "_metric."+metricID+"_dates."+dates},
-                        global: false //We don't want to show the loadingdiv.
+                            "_metric."+metricID+"_dates."+dates}
                     });
                 }
             }
@@ -195,8 +194,7 @@ function fillTable(){
                         },
                         url: "cgi-bin/metrics.py",
                         data: {cmd: "channelgrid", param: "channel."+rowIDs+
-                            "_metric."+metricID+"_dates."+dates},
-                        global: false //We don't want to show the loadingdiv.
+                            "_metric."+metricID+"_dates."+dates}
                     });
                 }
             }
