057d48dca46c34587c8fbe3d507d16e70483b83e
James Holland <jholland@usgs.gov>
2014-06-13 18:50:36 -0600

    Cleaned up the look and feel. Got the buttons semi working.

--- html/js/group.js
+++ html/js/group.js
@@ -5,94 +5,94 @@ group.js contains functions for displaying stations by groupings.
 License: Public Domain
 */
 
-var groupVis;
 function setupGroupTab(jTab, typesSorted){
     var eTab = $("<div id='tGroup' style=\"font-size: 12px\"></div>");
     jTab.find("ul").append('<li><a href="#tGroup">Groups</a></li>');
     jTab.append(eTab);
 
-        for(var i = 0; i < typesSorted.length; i++){
-            var gType = $("<div></div>");
-            gType.append($("<div>"+typesSorted[i]+"<div>"));
-            for(var t = 0; t<mapTIDtoGIDs[mapTNametoTID[typesSorted[i]]].length;t++){
-                var gID = mapTIDtoGIDs[mapTNametoTID[typesSorted[i]]][t];
-                var gName = mapGIDtoGName[mapTIDtoGIDs[mapTNametoTID[typesSorted[i]]][t]];
-                gType.append(createGroupCheckbox(gName, gID));
+    for(var i = 0; i < typesSorted.length; i++){
+        var gType = $("<div></div>");
+        gType.append($("<div>"+typesSorted[i]+"<div>"));
+        var maxCol = 12;
+        var colWidth = 140; //This size was left over from weight columns.
+        //Number of columns can be overrun by the numPerCol. It fills columns until no items remain.
+        var numCol = Math.floor($(document).width()/colWidth);
+        if(numCol > maxCol){  //We don't want more than 3 columns.
+            numCol = maxCol;
+        }
+        var numPerCol = Math.ceil((mapTIDtoGIDs[mapTNametoTID[typesSorted[i]]].length + 1)/numCol);
+        var curCol = -1; //If not -1 columns will skip 0
+        var columns = [];
+        var usedGroups = 0; //Tracks how many metrics are being displayed.
+        for(var t = 0; t<mapTIDtoGIDs[mapTNametoTID[typesSorted[i]]].length;t++){
+            if(!(usedGroups % numPerCol)){
+                curCol++;
+                columns.push($("<div style=\"display:table; border-spacing:10px;\"></div>"));
             }
-            eTab.append(gType);
+            var gID = mapTIDtoGIDs[mapTNametoTID[typesSorted[i]]][t];
+            var gName = mapGIDtoGName[mapTIDtoGIDs[mapTNametoTID[typesSorted[i]]][t]];
+            columns[curCol].append(createGroupCheckbox(gName, gID));
+            usedGroups++;
         }
-
-        /*
-    var maxCol = 8;
-    var colWidth = 460; //This size was left over from weight columns.
-    var numCol = Math.floor($(document).width()/colWidth);
-    if(numCol > maxCol){  //We don't want more than 3 columns.
-        numCol = maxCol;
+        var colTable = $("<div style=\"display:table; border-spacing: 9px;\"></div>");
+        for(var k = 0; k<columns.length; k++){
+            var col = $("<div style=\"display:table-cell;\"></div>");
+            col.append(columns[k]);
+            colTable.append(col);
+        }
+        gType.append(colTable);
+        eTab.append(gType);
     }
-    var numPerCol = Math.ceil((metrics.length + 1)/numCol);
-    var curCol = -1; //If not -1 columns will skip 0
-    var columns = [];
-    var usedGT = 0; //Tracks how many metrics are being displayed.
-    for(var curMetric = 0; curMetric < metrics.length; curMetric++){
-        if(!(usedMetrics % numPerCol)){
-            curCol++;
-            columns.push($("<div style=\"display:table; border-spacing:10px;\"></div>"));
-        }
-        if(metrics[curMetric].bVisible){  //If the column is hidden by default we don't want to let users view it. This applies to the groups column which is used for filtering.
-            columns[curCol].append(createColumnCheckbox(metrics[curMetric], curMetric));
-            usedMetrics++;
-        }
-    }
-    var colTable = $("<div style=\"display:table; border-spacing: 9px;\"></div>");
-    for(var i = 0; i<columns.length; i++){
-        var col = $("<div style=\"display:table-cell;\"></div>");
-        col.append(columns[i]);
-        colTable.append(col);
-    }*/
     eTab.append(
-        "<button type='button' id='btnCheckAll'>Show All</button>"
+        "<button type='button' id='btnCheckGroups'>Check All</button>"
     );
     eTab.append(
-        "<button type='button' id='btnUnCheckAll'>Hide All</button>"
+        "<button type='button' id='btnUnCheckGroups'>Uncheck All</button>"
+    );
+    eTab.append(
+        "<button type='button' id='btnFilterGroups'>Filter Table</button>"
     );
     bindColumnTab();
 }
 
 function createGroupCheckbox(label, groupID){
-    var cbdiv = $("<span id='groupCB"+groupID+"'/>");
+    var cbdiv = $("<div id='groupCB"+groupID+"'/>");
     cbdiv.append($("<input type='checkbox' checked='checked'/>"));
     cbdiv.append($("<label>"+label+"</label>"));
     return cbdiv;
 }
 
 function bindGroupTab(){
-    $("div[id^=metricCB]").each(function(){
+    $("div[id^=groupCB]").each(function(){
         $(this).find("label").on("click",function(){
             var checkbox = $(this).siblings("input[type=checkbox]");
             $(checkbox).prop("checked", !checkbox.prop("checked"));
-            setColVis(checkbox);
         });
         $(this).find("input[type=checkbox]").on("click",function(){
-            setColVis(this);
         });
     });
-    $("#btnCheckAll").on("click",function(){
-        $("div[id^=metricCB]").find("input[type=checkbox]").each(function(){
+    $("#btnCheckGroups").on("click",function(){
+        $("div[id^=groupCB]").find("input[type=checkbox]").each(function(){
             $(this).prop("checked", true);
-            setColVis(this);
         });
     });
-    $("#btnUnCheckAll").on("click",function(){
-        $("div[id^=metricCB]").find("input[type=checkbox]").each(function(){
+    $("#btnUnCheckGroups").on("click",function(){
+        $("div[id^=groupCB]").find("input[type=checkbox]").each(function(){
             $(this).prop("checked", false);
-            setColVis(this);
         });
     });
 }
 
-function setGroupVis(checkbox){
-    var colID = $(checkbox).parent().attr("id").slice(8);
-    dTable.fnSetColumnVis(colID, $(checkbox).prop("checked"));
+function filterGroups(){
+    var filterString = "" 
+    $("div[id^=groupCB]").each(function(){
+        $(this).find("label").on("click",function(){
+            var checkbox = $(this).siblings("input[type=checkbox]");
+            $(checkbox).prop("checked", !checkbox.prop("checked"));
+        });
+        $(this).find("input[type=checkbox]").on("click",function(){
+        });
+    });
 }
 
 function createGroupSelect(id){
