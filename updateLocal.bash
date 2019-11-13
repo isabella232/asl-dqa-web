008eb02c71996887167421b0bfd93ee5eb15e412
Adam M Baker <ambaker@usgs.gov>
2019-11-13 19:01:07 +0000

    SSL interception workaround

--- updateLocal.bash
+++ updateLocal.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 source venv/bin/activate
-pip install -r requirements.txt
+pip install -r requirements.txt --cert=/usr/share/ca-certificates/extra/DOIRootCA2.cer
 python manage.py migrate
