06d4832b58182f4bcb2058beec4416d8f65561c7
dwitte <dwitte@contractor.usgs.gov>
2020-04-16 14:28:02 -0600

    Assume these URLs are unique but can have parameters

--- dqa/urls.py
+++ dqa/urls.py
@@ -8,8 +8,8 @@ from dqa.views.index import index
 from dqa.views.summary import summary
 
 urlpatterns = [
-    url(r'^api/metrics$', metrics.metrics, name="metrics"),
-    url(r'^cgi-bin/dqaget.py$', dqaget.dqaget, name="dqaget"),
+    url(r'api/metrics', metrics.metrics, name="metrics"),
+    url(r'cgi-bin/dqaget.py', dqaget.dqaget, name="dqaget"),
     url(r'index.html', index.index, name="index"),
     url(r'(?P<group>[^/]+)/summary/$', summary.summary, name="summary"),
     url(r'summary/$', summary.summary, name="summary_nogroup"),
