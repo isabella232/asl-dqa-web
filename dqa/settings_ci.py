bf408f7d6c97aa65bedf06144a15b3a9e1439815
dwitte <dwitte@contractor.usgs.gov>
2020-05-19 15:45:55 -0600

    Add build config

---
+++ dqa/settings_ci.py
@@ -0,0 +1,23 @@
+"""
+Django settings for dqa test build.
+"""
+
+from .settings import *
+
+ECRET_KEY = 'uwh)=eqffff)f%u%pf^0z)h=7s&cf1_r$bsn!vyz4bmxzo0)77'
+
+ALLOWED_HOSTS = ['vmdevwb.cr.usgs.gov',
+                 'igskgacgvmdevwb.cr.usgs.gov',
+                 'localhost'
+                 ]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dqa_prod_clone',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'postgres',
+        'PORT': '5432',
+    }
+}
