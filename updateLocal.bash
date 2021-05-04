7210f107239860827c91a1c2d32199d725b15c43
Holland, James F <jholland@usgs.gov>
2021-05-04 18:33:48 +0000

    Update updateLocal.bash

--- updateLocal.bash
+++ updateLocal.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 source venv/bin/activate
-pip install -r requirements.txt --cert=/usr/share/ca-certificates/extra/DOIRootCA2.cer
+pip install -r requirements.txt
 python manage.py migrate
