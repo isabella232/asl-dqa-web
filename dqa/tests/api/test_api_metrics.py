e090e987a0b165cd19d156e46fb4f25b14872987
dwitte <dwitte@contractor.usgs.gov>
2021-11-17 09:58:08 -0700

    Fix API tests adding authorization test

--- dqa/tests/api/test_api_metrics.py
+++ dqa/tests/api/test_api_metrics.py
@@ -3,6 +3,9 @@ import json
 
 from django.test import TestCase
 from django.urls import reverse
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+from rest_framework.test import APIClient
 
 
 class Testmetrics(TestCase):
@@ -27,15 +30,28 @@ class Testmetrics(TestCase):
         response_reference = b'No parameters provided\n'
         self.assertEqual(response_reference, resp.content, msg='no parameter did not match')
 
+    @staticmethod
+    def create_token():
+        """ Generate an authorization token to test protected API paths """
+        test_user = User.objects.create_user(username='testuser1', password='123456')
+        token_object = Token.objects.create(user=test_user)
+        return token_object.key
+
     def test_metrics(self):
         output = {"model": "metric", "count": 3, "data": [{"id": 2, "name": "AvailabilityMetric", "display_name": "Availability", "description_short": "Returns a percentage of expected samples in the trace", "description_long": "For a sensors sample rate and the length of a full day, this metric compares the number of points gotten from the data archive with the expected number of samples it should have. This is returned as a percentage; if we have 1 point when we expected 2, this would be a 50% availability result.", "compute_type": "AVG_CH"}, {"id": 3, "name": "GapCountMetric", "display_name": "Gap Count", "description_short": "Returns the number of gaps found between data records for a sensors full-day trace.", "description_long": "This metric compares the start and end times of consecutive data records from a seed file for a sensors full-day data. If these records are more than a sample apart in time, then that is counted as a gap. The total number of gaps found is reported. Some metrics require a gapless trace, so if this value is positive, other metrics will have an empty result.", "compute_type": "VALUE_CO"}, {"id": 4, "name": "TimingQualityMetric", "display_name": "Timing Quality", "description_short": "Returns the average of the blockette 1001 timing records for the day", "description_long": "This metric takes the timing records for each blockette 1001 from a days SEED data and returns the average of these values.", "compute_type": "AVG_CH"}]}
         url = reverse('metrics')
-        post_resp = self.client.post(url, data=output, content_type='application/json')
+        # POST requires auth
+        apiclient = APIClient()
+        post_resp = apiclient.post(url, data=output, format='json')
+        self.assertEqual(post_resp.status_code, 401)
+        apiclient.credentials(HTTP_AUTHORIZATION='Token ' + self.create_token())
+        post_resp = apiclient.post(url, data=output, format='json')
         self.assertEqual(post_resp.status_code, 200)
         self.assertEqual({'status': 'ok', 'message': 'Metrics loaded'}, json.loads(post_resp.content.decode()), msg="metrics did not load")
+        # GET does not require auth
         resp = self.client.get(url)
         self.assertEqual(resp.status_code, 200)
-        response_reference = b'{"metrics": {"data": [{"id": 2, "name": "AvailabilityMetric", "display_name": "Availability", "description_short": "Returns a percentage of expected samples in the trace", "description_long": "For a sensors sample rate and the length of a full day, this metric compares the number of points gotten from the data archive with the expected number of samples it should have. This is returned as a percentage; if we have 1 point when we expected 2, this would be a 50% availability result.", "compute_type": "AVG_CH", "parent": null}, {"id": 3, "name": "GapCountMetric", "display_name": "Gap Count", "description_short": "Returns the number of gaps found between data records for a sensors full-day trace.", "description_long": "This metric compares the start and end times of consecutive data records from a seed file for a sensors full-day data. If these records are more than a sample apart in time, then that is counted as a gap. The total number of gaps found is reported. Some metrics require a gapless trace, so if this value is positive, other metrics will have an empty result.", "compute_type": "VALUE_CO", "parent": null}, {"id": 4, "name": "TimingQualityMetric", "display_name": "Timing Quality", "description_short": "Returns the average of the blockette 1001 timing records for the day", "description_long": "This metric takes the timing records for each blockette 1001 from a days SEED data and returns the average of these values.", "compute_type": "AVG_CH", "parent": null}], "count": 3}}'
+        response_reference = b'{"metrics":{"data":[{"id":2,"name":"AvailabilityMetric","display_name":"Availability","description_short":"Returns a percentage of expected samples in the trace","description_long":"For a sensors sample rate and the length of a full day, this metric compares the number of points gotten from the data archive with the expected number of samples it should have. This is returned as a percentage; if we have 1 point when we expected 2, this would be a 50% availability result.","compute_type":"AVG_CH","parent":null},{"id":3,"name":"GapCountMetric","display_name":"Gap Count","description_short":"Returns the number of gaps found between data records for a sensors full-day trace.","description_long":"This metric compares the start and end times of consecutive data records from a seed file for a sensors full-day data. If these records are more than a sample apart in time, then that is counted as a gap. The total number of gaps found is reported. Some metrics require a gapless trace, so if this value is positive, other metrics will have an empty result.","compute_type":"VALUE_CO","parent":null},{"id":4,"name":"TimingQualityMetric","display_name":"Timing Quality","description_short":"Returns the average of the blockette 1001 timing records for the day","description_long":"This metric takes the timing records for each blockette 1001 from a days SEED data and returns the average of these values.","compute_type":"AVG_CH","parent":null}],"count":3}}'
         self.assertEqual(response_reference, resp.content, msg='metrics did not read')
 
     def test_dates(self):
@@ -102,3 +118,4 @@ class Testmetrics(TestCase):
         self.assertEqual(resp.status_code, 200)
         response_reference = b'C,3353,LNZ,20,146\nC,3354,VMW,00,146\nC,3355,BH2,00,146\nC,3356,VMV,00,146\nC,3357,BH1,00,146\nC,3358,VMU,00,146\nC,3359,LH2,00,146\nC,3360,LH1,00,146\nC,3361,BHZ,00,146\nC,3362,VH2,00,146\nC,3363,VH1,00,146\nC,3364,LHZ,00,146\nC,3365,VHZ,00,146\nC,3366,LN2,20,146\nC,3367,LN1,20,146'
         self.assertEqual(response_reference, resp.content, msg='channels parameter did not match')
+
