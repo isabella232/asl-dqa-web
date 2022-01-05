66d43560a90c0ed50e468d374a843c8d0a08669e
dwitte <dwitte@contractor.usgs.gov>
2022-01-05 09:12:11 -0700

    Update scan documentation

--- metrics/models/util.py
+++ metrics/models/util.py
@@ -3,22 +3,27 @@ from django.db import models
 
 
 class Scan(models.Model):
+    """
+    Scan table
+    This table stores the scan parameters for seedscan to use in performing scans
+    crons inject the nightly and monthly scans
+    """
 
     id = models.BigAutoField(primary_key=True)
-    parent = models.ForeignKey('Scan', on_delete=models.DO_NOTHING)
-    last_updated = models.DateTimeField('Last Update', auto_now=True)
-    metric_filter = models.TextField('Metric Filter')
-    network_filter = models.TextField('Network Filter')
-    station_filter = models.TextField('Station Filter')
-    location_filter = models.TextField('Location Filter')
-    channel_filter = models.TextField('Channel Filter')
-    start_date = models.DateField('Start Date')
-    end_date = models.DateField('End Date')
-    priority = models.IntegerField('Priority', default=10)
-    delete_existing = models.BooleanField('Delete Exisiting', default=False)
-    scheduled_run = models.DateField('Scheduled Run Date')
-    finished = models.BooleanField('Scan Finished', default=False)
-    taken = models.BooleanField('Scan Taken', default=False)
+    parent = models.ForeignKey('Scan', on_delete=models.DO_NOTHING, help_text="None indicates root level, otherwise points to parent scan")
+    last_updated = models.DateTimeField('Last Update', auto_now=True, help_text="Last time this scan was modified")
+    metric_filter = models.TextField('Metric Filter', help_text="Limit scan to this metric")
+    network_filter = models.TextField('Network Filter', help_text="Limit scan to this network")
+    station_filter = models.TextField('Station Filter', help_text="Limit scan to this station")
+    location_filter = models.TextField('Location Filter', help_text="Limit scan to this location")
+    channel_filter = models.TextField('Channel Filter', help_text="Limit scan to this channel")
+    start_date = models.DateField('Start Date', help_text="Scan starting at this date")
+    end_date = models.DateField('End Date', help_text="Scan ends on this date")
+    priority = models.IntegerField('Priority', default=10, help_text="Scan priority, 100 highest. 0 forget it")
+    delete_existing = models.BooleanField('Delete Exisiting', default=False, help_text="Flag to delete existing scan data as you scan")
+    scheduled_run = models.DateField('Scheduled Run Date', help_text="Run scan in the future after this date")
+    finished = models.BooleanField('Scan Finished', default=False, help_text="Scan of this entry is finished")
+    taken = models.BooleanField('Scan Taken', default=False, help_text="Scan of this entry is in progress, scan engine uses multi-threading")
 
     class Meta:
         verbose_name = 'scan'
@@ -26,16 +31,18 @@ class Scan(models.Model):
 
 
 class ScanMessage(models.Model):
-
+    """
+    Messages attached to a scan, usually an error
+    """
     id = models.BigAutoField(primary_key=True)
-    scan = models.ForeignKey('Scan', on_delete=models.DO_NOTHING)
-    network = models.CharField('Network', max_length=2)
-    station = models.CharField('Station', max_length=10)
-    location = models.CharField('Location', max_length=10)
-    channel = models.CharField('Channel', max_length=10)
-    metric = models.CharField('Metric', max_length=50)
-    message = models.TextField('Message')
-    timestamp = models.DateTimeField('Timestamp', auto_now=True)
+    scan = models.ForeignKey('Scan', on_delete=models.DO_NOTHING, help_text="scan message applies to")
+    network = models.CharField('Network', max_length=2, help_text="network")
+    station = models.CharField('Station', max_length=10, help_text="station")
+    location = models.CharField('Location', max_length=10, help_text="location")
+    channel = models.CharField('Channel', max_length=10, help_text="channel")
+    metric = models.CharField('Metric', max_length=50, help_text="metric")
+    message = models.TextField('Message', help_text="Actual message")
+    timestamp = models.DateTimeField('Timestamp', auto_now=True, help_text="Timestamp for message creation")
 
     class Meta:
         verbose_name = 'scan message'
