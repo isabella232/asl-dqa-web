9b75267f603f2ffb0875703473f8c3fddf95a072
dwitte <dwitte@contractor.usgs.gov>
2022-02-02 11:53:15 -0700

    Fix typo

--- updateLocal.bash
+++ updateLocal.bash
@@ -4,5 +4,5 @@ pip install -r requirements.txt
 python manage.py migrate
 python manage.py migrate metrics --database=metrics
 # Load the new Django version of metrics table
-python manage.py load_mertrics_from_db
+python manage.py load_metrics_from_db
 deactivate
